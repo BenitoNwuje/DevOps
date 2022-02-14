ee1a472864f0410a465100b70772a12defe529ff
BenitoNwuje <47666166+BenitoNwuje@users.noreply.github.com>
2022-02-14 15:30:50 +0100

    resolve conflicts

--- program.sh
+++ program.sh
@@ -7,7 +7,9 @@ echo "Feature 2";
 read -p "Give your name: " name;
 
 echo $name;
->>>>>>> 5bd20d8 (Add print name functionality to program)
+(Add print name functionality to program)
 
 echo "Hello $name. Greetings to you my friend";
->>>>>>> 1e3c4a0 (Add personal greeting to program)
+(Add personal greeting to program)
+echo "Hello $name. Greetings to you my friend.";
+(Fix typo)
