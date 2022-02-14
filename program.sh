a121ea6d5f869dba92757b23a358dd761bb55c03
BenitoNwuje <47666166+BenitoNwuje@users.noreply.github.com>
2022-02-14 15:18:31 +0100

    add changes

--- program.sh
+++ program.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 echo "Hello World!";
-<<<<<<< HEAD
+
 echo "Feature 2";
-=======
+
 read -p "Give your name: " name;
+
 echo $name;
 >>>>>>> 5bd20d8 (Add print name functionality to program)
+
+echo "Hello $name. Greetings to you my friend";
+>>>>>>> 1e3c4a0 (Add personal greeting to program)
